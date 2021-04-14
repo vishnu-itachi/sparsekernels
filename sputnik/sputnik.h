e85abc57745116def3f141b6b3ee7e5ef1ad181d
vishnu <vishnu14july@gmail.com>
2021-04-14 19:42:37 +0530

    initial commit

---
+++ sputnik/sputnik.h
@@ -0,0 +1,12 @@
+#ifndef THIRD_PARTY_SPARSEKERNEL_SPARSEKERNEL_H_
+#define THIRD_PARTY_SPARSEKERNEL_SPARSEKERNEL_H_
+
+#include "sparsekernel/bias_relu/bias_relu.h"
+#include "sparsekernel/depthwise/cuda_depthwise.h"
+#include "sparsekernel/sddmm/cuda_sddmm.h"
+#include "sparsekernel/softmax/softmax.h"
+#include "sparsekernel/softmax/sparse_softmax.h"
+#include "sparsekernel/spmm/cuda_spmm.h"
+#include "sparsekernel/utils/index_format.h"
+
+#endif // THIRD_PARTY_SPARSEKERNEL_SPARSEKERNEL_H_
